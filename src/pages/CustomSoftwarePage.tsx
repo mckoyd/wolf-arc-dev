61d48bff5ad36c6b181f63d652f0997b78d01808
mckoyd <dominick.mckoy@gmail.com>
2024-04-22 18:32:28 -0400

    adds automation and ux section

--- src/pages/CustomSoftwarePage.tsx
+++ src/pages/CustomSoftwarePage.tsx
@@ -10,7 +10,10 @@ import Cash from "../assets/images/cash.svg";
 import StopWatch from "../assets/images/stopwatch.svg";
 import documentsAnimation from "../assets/animations/documentsAnimation/data";
 import scaleAnimation from "../assets/animations/scaleAnimation/data.json";
+import automationAnimation from "../assets/animations/automationAnimation/data.json";
+import uxAnimation from "../assets/animations/uxAnimation/data";
 import Lottie from "react-lottie";
+import Roots from "../assets/images/root.svg";
 
 const CustomSoftwarePage: FC = () => {
   const { classes } = useCustomSoftwarePageStyles();
@@ -28,6 +31,24 @@ const CustomSoftwarePage: FC = () => {
     loop: true,
     autoplay: true,
     animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const automationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: automationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const uxOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: uxAnimation,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
@@ -143,8 +164,8 @@ const CustomSoftwarePage: FC = () => {
         </Grid>
       </Grid>
 
-      <Grid item container>
-        <Grid item container className={classes.itemContainer}>
+      <Grid item container justifyContent={"space-around"}>
+        <Grid item container className={classes.itemContainer} md>
           <Grid item container direction="column" md>
             <Grid item>
               <Typography variant="h4">Digital Documents & Data</Typography>
@@ -169,12 +190,12 @@ const CustomSoftwarePage: FC = () => {
           <Grid item md>
             <Lottie
               options={documentsOptions}
-              style={{ maxWidth: 275, maxHeight: 325, minHeight: 275 }}
+              style={{ maxWidth: 275, maxHeight: 275, minHeight: 250 }}
             />
           </Grid>
         </Grid>
 
-        <Grid item container className={classes.itemContainer}>
+        <Grid item container className={classes.itemContainer} md>
           <Grid item md>
             <Lottie
               options={scaleOptions}
@@ -192,6 +213,97 @@ const CustomSoftwarePage: FC = () => {
                 Whether you’re a large brand, just getting started, or taking
                 off right now, our application architecture ensures pain-free
                 growth and reliability.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        justifyContent={"center"}
+        style={{ margin: "20em 0" }}
+      >
+        <Grid item container direction={"column"} alignItems={"center"}>
+          <Grid item>
+            <img
+              src={Roots}
+              alt={"tree with roots extending out"}
+              height={"450em"}
+              width={"450em"}
+            />
+          </Grid>
+        </Grid>
+        <Grid item className={classes.itemContainer}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Root Cause Analysis
+          </Typography>
+          <Typography variant="body1" align="center" paragraph>
+            Many problems are merely symptoms of larger, underlying issues.
+          </Typography>
+          <Typography variant="body1" align="center" paragraph>
+            We can help you thoroughly examine all areas of your business to
+            develop a holistic plan for the most effective implementation of
+            technology.
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid item container justifyContent={"space-around"}>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Why waste time when you don’t have to?
+              </Typography>
+              <Typography variant="body1" paragraph>
+                We can help you identify processes with time or event based
+                actions which can now easily be automated.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Increasing efficiency increases profits, leaving you more time
+                to focus on your business, not busywork.
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item md>
+            <Lottie
+              options={automationOptions}
+              style={{ maxWidth: 275, maxHeight: 275, minHeight: 250 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item md>
+            <Lottie
+              options={uxOptions}
+              style={{ maxWidth: 280, maxHeight: 260 }}
+            />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="right">
+                User Experience Design
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph align="right">
+                A good design that isn’t usable isn’t a good design.
+              </Typography>
+              <Typography variant="body1" paragraph align="right">
+                So why are so many pieces of software complicated, confusing,
+                and frustrating?
+              </Typography>
+              <Typography variant="body1" paragraph align="right">
+                By prioritizing users and the real ways they interact with
+                technology we’re able to develop unique, personable experiences
+                that solve problems rather than create new ones.
               </Typography>
             </Grid>
           </Grid>
