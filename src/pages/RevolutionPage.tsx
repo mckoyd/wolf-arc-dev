2da782b3d450af50aa60498adea5aa90b426779e
mckoyd <dominick.mckoy@gmail.com>
2024-04-26 15:21:15 -0400

    adds consultation section for process block on rev pg

--- src/pages/RevolutionPage.tsx
+++ src/pages/RevolutionPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Typography } from "@mui/material";
 import { useRevolutionPageStyles } from "./RevolutionPage";
 import VisionImg from "../assets/images/vision.svg";
+import ConsultationImg from "../assets/images/consultationIcon.svg";
 import technologyAnimation from "../assets/animations/technologyAnimation/data.json";
 import Lottie from "react-lottie";
 
@@ -150,6 +151,72 @@ const RevolutionPage: React.FC = () => {
               style={{ maxWidth: "40em", margin: 0 }}
             />
           </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        justifyContent={"center"}
+        className={classes.rowContainer}
+      >
+        <Grid item>
+          <Typography variant="h4" gutterBottom>
+            Process
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{ background: "#b3b3b3", height: "90em" }}
+      >
+        <Grid item container direction="column" lg>
+          <Grid item>
+            <Typography
+              variant="h4"
+              gutterBottom
+              style={{ color: "#000", marginTop: "5em" }}
+            >
+              Consultation
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{ color: "#fff", maxWidth: "20em" }}
+              paragraph
+            >
+              Our process begins the moment you realize you need a piece of
+              technology for your business. Whether you already have an idea for
+              where to start and what to do, or if you just know you want to
+              step things up, our initial consultation will help you examine
+              your business holistically to find the best solutions.
+            </Typography>
+            <Typography
+              variant="body1"
+              style={{ color: "#fff", maxWidth: "20em" }}
+              paragraph
+            >
+              Detailed notes will be taken on your requirements and constraints,
+              while taking care to identify other potential areas for
+              consideration.
+            </Typography>
+            <Typography
+              variant="body1"
+              style={{ color: "#fff", maxWidth: "20em" }}
+              paragraph
+            >
+              Cutting-edge advancements in machine learning like object
+              detection and natural language processing allow computers to do
+              things previously unimaginable, and our expertise and intuition
+              will help usher you into this new future of possibilities.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item lg style={{ alignSelf: "center" }}>
+          <img src={ConsultationImg} alt="handshake" />
         </Grid>
       </Grid>
     </Grid>
