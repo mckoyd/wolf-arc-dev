86fbcdd0b8bfef93e2658bf07db6d892c0eccc9d
mckoyd <dominick.mckoy@gmail.com>
2024-04-29 15:20:41 -0400

    adds call to action content on bg

--- src/pages/ContactPage.config.ts
+++ src/pages/ContactPage.config.ts
@@ -1,5 +1,6 @@
 import { tss } from "tss-react/mui";
 import BackgroundImg from "../assets/images/background.jpg";
+import { CSSProperties } from "react";
 
 export const useContactPageStyles = tss.create(({ theme }) => ({
   bg: {
@@ -8,5 +9,28 @@ export const useContactPageStyles = tss.create(({ theme }) => ({
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     height: "60em",
+  },
+  callToActionBtn: {
+    ...(theme.typography.learnBtn as CSSProperties),
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
+  },
+  estimateBtn: {
+    ...(theme.typography.estimateBtn as CSSProperties),
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.orange,
+    fontSize: "1.5rem",
+    marginRight: "5em",
+    marginLeft: "2em",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
   },
 }));
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,12 +1,54 @@
-import { Grid, Typography, useTheme } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import React, { useState } from "react";
 import { useContactPageStyles } from "./ContactPage.config";
 import PhoneIcon from "../assets/images/phone.svg";
 import EmailIcon from "../assets/images/email.svg";
+import AirplaneIcon from "../assets/images/send.svg";
+import { Link } from "react-router-dom";
+import ButtonArrow from "../components/ButtonArrow";
 
 const ContactPage: React.FC = () => {
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+
   const { classes } = useContactPageStyles();
+
+  const [name, setName] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const handleNameChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setPhone(e.target.value);
+  };
+
+  const handleMessageChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <Grid item container style={{ marginTop: "-1em" }}>
       <Grid item container direction="column" justifyContent={"center"} lg={3}>
@@ -51,8 +93,101 @@ const ContactPage: React.FC = () => {
             </Typography>
           </Grid>
         </Grid>
+        <Grid item container>
+          <Grid item>
+            <TextField
+              label="Name"
+              id="name"
+              variant="standard"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Email"
+              id="email"
+              variant="standard"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Phone"
+              id="phone"
+              variant="standard"
+              value={phone}
+              onChange={handlePhoneChange}
+            />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <TextField
+            value={message}
+            id="message"
+            onChange={handleMessageChange}
+            variant="standard"
+            multiline
+            rows={10}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained">
+            Send Message <img src={AirplaneIcon} alt="airplane" />
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item container className={classes.bg} lg={9}></Grid>
+      <Grid item container className={classes.bg} lg={9} alignItems={"center"}>
+        <Grid
+          item
+          style={{
+            marginLeft: matchesSM ? 0 : "5em",
+            textAlign: matchesSM ? "center" : "inherit",
+          }}
+        >
+          <Grid container direction={"column"}>
+            <Grid item>
+              <Typography variant="h2">
+                Simple Software.
+                <br /> Revolutionary Results.
+              </Typography>
+              <Typography variant="subtitle2" style={{ fontSize: "1.5rem" }}>
+                Take advantage of the 21st Century.
+              </Typography>
+              <Grid
+                container
+                justifyContent={matchesSM ? "center" : undefined}
+                item
+              >
+                <Button
+                  variant="outlined"
+                  className={classes.callToActionBtn}
+                  component={Link}
+                  to="/revolution"
+                >
+                  <span style={{ marginRight: 5 }}>Learn More</span>
+                  <ButtonArrow
+                    width="15"
+                    height="15"
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            component={Link}
+            to="/estimate"
+            className={classes.estimateBtn}
+          >
+            Free Estimate
+          </Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
